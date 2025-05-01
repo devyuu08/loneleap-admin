6d9247e932f99079f464fbccb73dcc0464b43720
Yujin Jung <devyuu08@gmail.com>
2025-05-01 11:07:11 +0900

    feat: Firebase Storage 설정 및 이미지 업로드 훅 구현

--- pages/admin/recommendation/new.js
+++ pages/admin/recommendation/new.js
@@ -2,8 +2,16 @@ import React from "react";
 import AdminProtectedRoute from "@/components/auth/AdminProtectedRoute";
 import AdminLayout from "@/components/layout/AdminLayout";
 import RecommendationForm from "@/components/recommendation/RecommendationForm";
+// import { useAddRecommendation } from "@/hooks/useAddRecommendation";
 
 export default function NewRecommendationPage() {
+  // const { addRecommendation, loading } = useAddRecommendation();
+
+  // const handleSubmit = async (formData) => {
+  //   await addRecommendation(formData);
+  //   // 성공 후 이동 또는 알림
+  // };
+
   return (
     <AdminProtectedRoute>
       <AdminLayout>
