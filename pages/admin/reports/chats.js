0b9301400d651220945c61614c326c624ac8e829
Yujin Jung <devyuu08@gmail.com>
2025-06-08 15:14:22 +0900

    docs: 리뷰/채팅 신고 페이지 컴포넌트 주석 정비

--- pages/admin/reports/chats.js
+++ pages/admin/reports/chats.js
@@ -1,5 +1,11 @@
 import AdminChatReportsContainer from "@/components/reports/chat/AdminChatReportsContainer";
 
+/**
+ * AdminChatReportsPage
+ * - 채팅 신고 목록 페이지
+ * - 서버사이드 데이터를 컨테이너에 전달
+ */
+
 function AdminChatReportsPage(props) {
   return <AdminChatReportsContainer {...props} />;
 }
