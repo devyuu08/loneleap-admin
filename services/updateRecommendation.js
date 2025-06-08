f0dd5d58eded08ce663117f2d9a3324da01ee7fc
Yujin Jung <devyuu08@gmail.com>
2025-06-08 15:17:56 +0900

    docs: 서비스 함수 파일 JS Doc 주석 추가

--- services/updateRecommendation.js
+++ services/updateRecommendation.js
@@ -1,6 +1,14 @@
 import { doc, updateDoc, serverTimestamp } from "firebase/firestore";
 import { db } from "@/lib/firebase/client";
 
+/**
+ * Firestore의 추천 여행지 문서를 업데이트합니다.
+ *
+ * @param {string} id - 수정할 문서의 ID
+ * @param {Object} data - 수정할 데이터
+ * @returns {Promise<void>}
+ */
+
 export async function updateRecommendationInFirestore(id, data) {
   const ref = doc(db, "recommended_places", id);
 
