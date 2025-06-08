8ce7e55e1ed227ef9c5d6a022372d17ed8eafef0
Yujin Jung <devyuu08@gmail.com>
2025-06-08 15:14:41 +0900

    docs: 사용자 관리 및 대시보드 페이지 컴포넌트 주석 정비

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,6 +1,12 @@
 import { getAdminDashboardData } from "@/lib/server/dashboard/getAdminDashboardData";
 import AdminDashboardContainer from "@/components/dashboard/AdminDashboardContainer";
 
+/**
+ * AdminDashboardPage
+ * - 관리자 대시보드 페이지 (SSR)
+ * - 통계 카드, 차트, 신고 목록 등을 렌더링
+ */
+
 export const getServerSideProps = getAdminDashboardData;
 
 function AdminDashboardPage(props) {
--- pages/admin/users/index.js
+++ pages/admin/users/index.js
@@ -1,5 +1,11 @@
 import UserTableContainer from "@/components/users/UserTableContainer";
 
+/**
+ * AdminUsersPage
+ * - 사용자 관리 테이블 페이지
+ * - 사용자 목록 및 상태 관리 기능 제공
+ */
+
 function AdminUsersPage() {
   return (
     <div className="p-6">
